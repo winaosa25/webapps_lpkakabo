6277cc156c997af388bec2963333f6bf213e4d0f
winaosa25 <130086780+winaosa25@users.noreply.github.com>
2023-04-24 12:24:30 +0700

    Update webapk1.py

--- webapk1.py
+++ webapk1.py
@@ -21,14 +21,17 @@ st.title('Aplikasi Perhitungan Kadar(%b/v)')
 normalitas = st.number_input('Masukan Nilai Normalitas Titran (N)')
 volume = st.number_input('Masukan Nilai Volume Titran (mL)')
 be = st.number_input('Masukan nilai BE')
-volumee = st.number_input('Masukan Nilai Volume Titrat (mL)')
 fp = st.number_input('Masukkan Nilai FP')
+volumee = st.number_input('Masukan Nilai Volume Titrat (mL)')
 
 tombol = st.button('Hitung Nilai Kadar(%(b/v))')
 
 if tombol:
     nilai_persentase = volume*normalitas*be*fp*10**-3*100/(volumee) 
     st.success(f'Nilai Normalitas adalah {nilai_persentase}')
+    
+import streamlit as st
+
 st.title('Aplikasi Perhitungan Kadar(%b/b)')
 
 normalitas = st1.number_input('Masukan Nilai Normalitas Titran (N)')
