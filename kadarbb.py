8d1c7d8408137379d62e5d7db63f2e8f2a06ca9c
winaosa25 <130086780+winaosa25@users.noreply.github.com>
2023-04-24 13:51:04 +0700

    Update kadarbb.py

--- kadarbb.py
+++ kadarbb.py
@@ -1,15 +1,15 @@
 import streamlit as st
 
-st.title('Aplikasi Perhitungan Kadar(%b/b)')
+st.title("Aplikasi Perhitungan Kadar(%b/b)")
 
-normalitas = st1.number_input('Masukan Nilai Normalitas Titran (N)')
-volume = st1.number_input('Masukan Nilai Volume Titran (mL)')
-be = st1.number_input('Masukan nilai BE')
-gram = st1.number_input('Masukan Nilai Gram Contoh (g)')
-fp = st.number_input('Masukkan Nilai FP')
+normalitas = st.number_input("Masukan Nilai Normalitas Titran (N)")
+volume = st.number_input("Masukan Nilai Volume Titran (mL)")
+be = st.number_input("Masukan nilai BE")
+gram = st.number_input("Masukan Nilai Gram Contoh (g)")
+fp = st.number_input("Masukkan Nilai FP")
 
-tombol = st.button('Hitung Nilai Kadar(%(b/b))')
+tombol = st.button("Hitung Nilai Kadar(%(b/b))")
 
 if tombol:
     nilai_persentase = volume*normalitas*be*fp*10**-3*100/(gram) 
-    st.success(f'Nilai Normalitas adalah {nilai_persentase}')
+    st.success(f"Nilai Kadar (%(b/b)) adalah {nilai_persentase}")
