7905c8958d1cc07625f999793cc25221bd124bff
winaosa25 <130086780+winaosa25@users.noreply.github.com>
2023-04-24 13:03:22 +0700

    Update webapk3.py

--- webapk3.py
+++ webapk3.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.title('Aplikasi Perhitungan Normalitas(N) dan Kadar(%)')
-st.title('Aplikasi Perhitungan Normalitas(N)')
 
+st.title('Aplikasi Perhitungan Normalitas(N)')
 bobot = st.number_input('Masukan Nilai Bobot Standar Primer (mg)')
 volume = st.number_input('Masukan Nilai Volume Standar Sekunder (mL)')
 be = st.number_input('Masukan nilai BE Standar Primer')
@@ -16,16 +16,15 @@ if tombol:
 
 import streamlit as st
 
-st.title('Aplikasi Perhitungan Kadar(%b/v)')
-
+st.title('Aplikasi Perhitungan Kadar(%)')
 normalitas = st.number_input('Masukan Nilai Normalitas Titran (N)')
 volume = st.number_input('Masukan Nilai Volume Titran (mL)')
 be = st.number_input('Masukan nilai BE')
-fp = st.number_input('Masukkan Nilai FP')
+efpe = st.number_input('Masukkan Nilai FP')
 volumee = st.number_input('Masukan Nilai Volume Titrat (mL) atau Gram Contoh (g)')
 
 hitung = st.button('Hitung Nilai Kadar(%)')
 
 if hitung:
-    nilai_persentase = volume*normalitas*be*fp*10**-3*100/(volumee) 
+    nilai_persentase = volume*normalitas*be*efpe*10**-3*100/(volumee) 
     st.success(f'Nilai Kadar (%) adalah {nilai_persentase}')
